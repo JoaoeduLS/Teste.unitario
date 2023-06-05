f8c34c19caca8701796c8a040ae44b6a059de4ee
JoaoeduLS <joaoeduardo.lsilva@mgail.com>
2023-06-05 02:33:06 -0300

    arrumando erros feito no codigo e adicionando o mediaCAtest para realiza a ativiadade

--- mediaCA.js
+++ mediaCA.js
@@ -1,17 +1,19 @@
 function mediaCA(notaDoAluno) {
   if (notaDoAluno >= 9.0 && notaDoAluno <= 10) {
-    console.log("SS");
-  } else if (notaDoAluno >= 7.0 && notaDoAluno < 9) {
-    console.log("MS");
+    return "SS - Superior";
+  } else if (notaDoAluno >= 7.0 && notaDoAluno < 9.0) {
+    return "MS - Médio Superior";
   } else if (notaDoAluno >= 5.0 && notaDoAluno < 7.0) {
-    console.log("MM");
+    return "MM - Médio";
   } else if (notaDoAluno >= 3.0 && notaDoAluno < 5.0) {
-    console.log("MI");
+    return "MI - Médio Inferior";
   } else if (notaDoAluno >= 0.1 && notaDoAluno < 3.0) {
-    console.log("II");
-  } else if (notaDoAluno == 0) {
-    console.log("SR");
+    return "II - Inferior";
+  } else if (notaDoAluno === 0.0) {
+    return "SR - Sem rendimento";
   } else {
-    console.log("O aluno nao possui nota");
+    throw new Error("nota inválida");
   }
 }
+
+module.exports = mediaCA;
